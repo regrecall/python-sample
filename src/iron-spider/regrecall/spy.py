ad0e62783d937eec4dc0ac44db26da07a3028aa3
regrecall <regrecall@regrecalls-MacBook-Pro.local>
2016-02-05 10:52:36 +0800

    get title

--- src/iron-spider/regrecall/spy.py
+++ src/iron-spider/regrecall/spy.py
@@ -1,4 +1,23 @@
-from urllib2 import urlopen
+from urllib2 import urlopen, HTTPError
+from bs4 import BeautifulSoup
 
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-print(html.read())
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        print(e)
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read(), "html.parser")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        print(e)
+        return None
+    return title
+
+
+title = getTitle("http://www.pythonscraping.com/pages/page1.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
