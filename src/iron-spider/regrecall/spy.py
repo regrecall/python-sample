ca959d92d6679f54293fe04e4156a41fe7b3fcc3
regrecall <regrecall@regrecalls-MacBook-Pro.local>
2016-02-23 20:32:33 +0800

    spy

--- src/iron-spider/regrecall/spy.py
+++ src/iron-spider/regrecall/spy.py
@@ -1,23 +1,43 @@
 from urllib2 import urlopen, HTTPError
 from bs4 import BeautifulSoup
 
-def getTitle(url):
-    try:
-        html = urlopen(url)
-    except HTTPError as e:
-        print(e)
-        return None
-    try:
-        bsObj = BeautifulSoup(html.read(), "html.parser")
-        title = bsObj.body.h1
-    except AttributeError as e:
-        print(e)
-        return None
-    return title
+# def getTitle(url):
+#     try:
+#         html = urlopen(url)
+#     except HTTPError as e:
+#         print(e)
+#         return None
+#     try:
+#         bsObj = BeautifulSoup(html.read(), "html.parser")
+#         title = bsObj.body.h1
+#     except AttributeError as e:
+#         print(e)
+#         return None
+#     return title
+#
+#
+# title = getTitle("http://www.pythonscraping.com/pages/page1.html")
+# if title == None:
+#     print("Title could not be found")
+# else:
+#     print(title)
+#
+# htmlTag = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
+# bsTag = BeautifulSoup(htmlTag.read(), "html.parser")
+#
+# tagList = bsTag.findAll("span", {"class": "green"})
+# for name in tagList:
+#     print (name.get_text())
+#
+# nameList = bsTag.findAll(text="the prince")
+# print(len(nameList))
+#
+htmlChildren = urlopen("http://www.pythonscraping.com/pages/page3.html")
+bsChildren = BeautifulSoup(htmlChildren.read(), "html.parser")
 
+# for child in bsChildren.find("table", {"id": "giftList"}).children:
+#     print(child)
 
-title = getTitle("http://www.pythonscraping.com/pages/page1.html")
-if title == None:
-    print("Title could not be found")
-else:
-    print(title)
+for sibling in bsChildren.find("table", {"id": "giftList"}).tr.next_siblings:
+    print(sibling)
+
